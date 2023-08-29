848ce6700b3f083aeba69b403f3df64779ecb915
JhulyVivas <131303835+JhulyVivas@users.noreply.github.com>
2023-08-29 18:58:45 -0500

    POST

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import {sendNotification,startNotificationListener} from "./listener.mjs";
 
 const app = express();
 
@@ -14,6 +15,15 @@ app.get("/", (req, res) => {
 app.get("/test/:nombre", (req, res) => {
     res.send("test "+ req.params.nombre);
 });
+
+// Ruta para el endpoint POST
+app.post('/', (req, res) => {
+    //console.log(req.body)
+    sendNotification(req.body);
+    res.send("Peticion POST");
+});
+
+startNotificationListener();
 
 app.listen(port, () => {
     console.log("http:localhost:3000");
